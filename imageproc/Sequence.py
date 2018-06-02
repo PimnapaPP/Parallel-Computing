792afdb0d2ecdb34b7176cc1bb9e3576100dccda
PimnapaPP <37994920+PimnapaPP@users.noreply.github.com>
2018-06-02 19:57:34 +0700

    For python version 2.7

--- imageproc/Sequence.py
+++ imageproc/Sequence.py
@@ -12,7 +12,7 @@ import time
 posX = 20
 posY = 20
 
-# detect "pepsi" object function-------------------------------------------------
+# detect "pepsi" object function------------------------------------------------
 def detect4skal(hsv):
     global posX, posY
     global pepsi
